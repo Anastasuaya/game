611a6b4cdbd50a2833555acb788578a48feb287a
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-07-26 01:09:27 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,39 @@
 import './style.css'
 
 // Получаем элемент, который будет прыгать 
-// const catGirl = document.getElementById('catGirl') as HTMLImageElement
-
-
-
-// Получаем элемент, который будет прыгать 
 const jumpingElement = document.getElementById('jumpingElement') as HTMLDivElement
 
+    jumpingElement.style.marginTop = '100px'
+
+ 
 // Функция для обработки нажатия клавиши 
 document.addEventListener('keydown' ,(e) => { 
- // Проверяем, что нажата клавиша пробела 
- if (e.code === 'Space') { 
- // Добавляем класс для анимации прыжка 
- jumpingElement.classList.add('jump'); 
- 
- // Удаляем класс после завершения анимации 
- setTimeout(() => { 
- jumpingElement.classList.remove('jump'); 
- }, 500); // Длительность анимации в миллисекундах (здесь 500 мс) 
- } 
-}  )
- 
+    let jumpingCharsCoords = jumpingElement.getBoundingClientRect()
+    let x = jumpingCharsCoords.x
+    let y = jumpingCharsCoords.y
+
+    if (x > document.documentElement.clientWidth - jumpingElement.offsetWidth) x = document.documentElement.clientWidth - jumpingElement.offsetWidth
+    if (x < 0) x = 0
+    if (y < 0) y = 0
+    if (y > document.documentElement.clientHeight - jumpingElement.offsetHeight) y = document.documentElement.clientHeight - jumpingElement.offsetHeight
+    if (e.code == 'ArrowRight') {
+        x += 50
+        jumpingElement.style.left = x + "px"
+    }  else if (e.code == 'ArrowLeft') {
+        x -= 50
+        jumpingElement.style.left = x + "px"
+    }  else if (e.code == 'Space') { 
+        e.preventDefault()
+        // Добавляем класс для анимации прыжка 
+        jumpingElement.classList.add('jump')
+        // Удаляем класс после завершения анимации 
+        setTimeout(() => {jumpingElement.classList.remove('jump') }, 500) // Длительность анимации в миллисекундах 
+    } 
+
+})
+
+
+
 // document.addEventListener ('keydown', (e) => {
 
 //     let catGirlCharsCoords = catGirl.getBoundingClientRect()
