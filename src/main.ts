0106a04b1144845a742eb2c3626f3c84387319ca
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-07-30 18:47:44 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -52,15 +52,16 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => { jumpingElement.classList.remove('jump') }, 500) // Длительность анимации в миллисекундах 
       }
 
-      // Обработчик события клика на кнопку "Нет"
-      noButton.addEventListener('click', () => {
-        // modal.style.display = 'none' // Скрыть модальное окно
-        // const newWindow = window.open("", "new window", "")
-        // newWindow.document.write("<p>Hello, World!</p>")
-      })
+     
     })
 
   })
+   // Обработчик события клика на кнопку "Нет"
+      noButton.addEventListener('click', () => {
+        modal.style.display = 'none' // Скрыть модальное окно
+        const newWindow = window.open("", "new window", "")
+        newWindow.document.write("<p>Иди играть</p>")
+      })
 })
 
 
