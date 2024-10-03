39715c62459ad6eb47018c610c26c57cfd6c10bc
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-10-03 20:45:07 +0300

    so

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import kaboom from 'kaboom'
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 import './style.css'
 
+const app = createApp(App)
+app.mount('#app')
 
-kaboom()
+
+
 
